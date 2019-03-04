6e0b2a6437d7eecdc508d04caf8cc7774bf6468c
Tanguy Cogrel <COGREL.T@ADMINISTRATION.LOCAL>
2019-03-04 16:55:24 +0100

    Initial commit

---
+++ GameState.gd
@@ -0,0 +1,19 @@
+extends Node
+
+var num_levels = 27
+var current_level = 1
+
+var game_scene = 'res://Main.tscn'
+var title_screen = 'res://ui/TitleScreen.tscn'
+var end_screen = 'res://ui/EndScene.tscn'
+
+func restart():
+	current_level = 1
+	get_tree().change_scene(title_screen)
+
+func next_level():
+	current_level += 1
+	if current_level <= num_levels:
+		get_tree().reload_current_scene()
+	elif current_level > num_levels:
+		get_tree().change_scene(end_screen)
